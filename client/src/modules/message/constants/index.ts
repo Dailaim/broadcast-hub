8b3dda93bcaaabae289deafb3b00eeaad0cf0e2a
Leonardo Iglesias <laiglesias.min@gmail.com>
2025-04-14 07:41:29 -0500

    chore: config test deploys

--- client/src/modules/message/constants/index.ts
+++ client/src/modules/message/constants/index.ts
@@ -1,3 +1,3 @@
 export const AVAILABLE_GROUPS = ["Group A", "Group B", "Test Numbers"] as const;
 
-export const API_URL = "http://localhost:8080";
+export const API_URL = import.meta.env.VITE_API_URL;
--- server/main.py
+++ server/main.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv("../.env")
 
 from fastapi import FastAPI
 import uvicorn
